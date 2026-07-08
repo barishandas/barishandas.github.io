f96d7ca8434c07a978ad4dbd3f8682f246d445c0
barishandas <68078963+barishandas@users.noreply.github.com>
2026-07-08 10:17:29 +0000

    Deploying to gh-pages from @ barishandas/barishandas.github.io@9a301bab27bd29bde81cd5fda446247d41a71cb9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,35 +438,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%61%73%62%61%72%69%73%68%61%6E@%67%6D%61%69%6C.%63%6F%6D,%62%61%72%69%73%68%61%6E.%64%61%73@%66%65%6D%74%6F-%73%74.%66%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/barishan-das-6828a81b8", "_blank");
         },
       },{
       id: 'light-theme',
